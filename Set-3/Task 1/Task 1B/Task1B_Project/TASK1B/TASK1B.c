854d614f1dc3f5daae48b15a2c96249e6304f531
KARTHEEKCIC <bkartheekreddy@gmail.com>
2017-11-23 23:07:04 +0530

    updating the ReachDestinationAvoidingNode function to build graph only once and adding comments

--- Set-3/Task 1/Task 1B/Task1B_Project/TASK1B/TASK1B.c
+++ Set-3/Task 1/Task 1B/Task1B_Project/TASK1B/TASK1B.c
@@ -18,6 +18,9 @@ int start = 45;
 //global present direction to store the present direction at which the firebird is pointing
 int present_direction = 1;
 
+//to check whether the graph is intialized or not
+_Bool is_initialized = 0;
+
 /** Structure Name: Direction
  ** Purpose: Used to store the directions to be traversed by the firebird. It has three values -1,0,1 and additional value of 2 to stop.
  **          -1 - left, 0 - straight, 1 - right, 2 - destination has been reached.
@@ -26,7 +29,12 @@ struct Direction {
 	int directions[X];
 };
 
-
+/** This is our own function to determine the firebirds movements on the flex according to the path calculated
+ ** Function Name: moveInThePath 
+ ** Input: path - it is the structure representing the shortest path from the start to the finish
+ ** Output: Returns a Direction structure which consists of the directions to be taken by the firebird V in order to move
+ ** in the path given as argument
+ **/
 struct Direction moveInThePath(struct Path_Array path) {
 	
 	int next_direction;
@@ -37,6 +45,9 @@ struct Direction moveInThePath(struct Path_Array path) {
 	struct Direction direction;
 
 	while(next_node!=-1) {
+		// calculating the next direction to be taken. 
+		// The following schema maps the values of next_direction to directions (north, south, east, west) -
+		// 0 - west | 1 - north | 2 - east | 3-south
 		if(next_node - present_node == 1) {
 			next_direction = 2;
 		} else if(next_node - present_node == -1) {
@@ -47,6 +58,9 @@ struct Direction moveInThePath(struct Path_Array path) {
 			next_direction = 1;
 		}
 
+		// direction to be taken with respect to the present node in order to move to the next node.
+		// The following schema maps the values of turn to left, right and straight - 
+		// -1,3 - left | 1,-3 - right | 0 - straight
 		int turn = next_direction - present_direction;
 
 		if(turn == -1 || turn == 3) {
@@ -71,9 +85,17 @@ struct Direction moveInThePath(struct Path_Array path) {
 void ReachDestinationAvoidingNode(unsigned char Xd,unsigned char Yd,unsigned char Xn,unsigned char Yn)
 {
 	//TODO:: Please write your application code. You can make your own functions & header files
+	
+	//declaring the graph
+	static struct Graph graph;
 
-	//creating the graph and removing the obstacle from it
-	struct Graph graph = createGraph();
+	//creating the graph if it not created and initialized yet
+	if(!is_initialized) {
+		graph = createGraph();
+		is_initialized = 1;
+	}
+
+	//removing the obstacle from it
 	graph = removeObstacle(graph,Xn,Yn);
 
 	//mapping the coordinates of the destination node on the flex to the node number in the graph
@@ -88,6 +110,12 @@ void ReachDestinationAvoidingNode(unsigned char Xd,unsigned char Yd,unsigned cha
 	//call the function to run the firebird V on the specified shortest path.
 	//localization of the firebird V
 	struct Direction directions = moveInThePath(path_array);
+
+	//starting the motion of the bot
+
+
+	//adding the obstacle
+	graph = addObstacle(graph,Xn,Yn);
 
 }
 
